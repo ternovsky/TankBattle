85d564efac5d7ebfb45432a389f3464dca628778
ternovsky <aleksey.ternovsky@gmail.com>
2012-11-15 22:50:52 +0400

    Hello World

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.logging.Logger;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ternovsky
+ * Date: 15.11.12
+ * Time: 22:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+    public static final Logger LOGGER = Logger.getLogger(Main.class.getSimpleName());
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello World!");
+    }
+}
